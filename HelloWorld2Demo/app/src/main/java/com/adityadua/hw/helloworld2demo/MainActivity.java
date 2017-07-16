274bbc97dcf1485f3bf924cd0f3f7fa8903e2077
AdityaDua <adityaduatechm@gmail.com>
2017-07-16 11:30:15 +0530

    Initial commit

---
+++ HelloWorld2Demo/app/src/main/java/com/adityadua/hw/helloworld2demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.adityadua.hw.helloworld2demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Multiple phones of differnt Config & like RAM , different Screen size */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+
+
+}
